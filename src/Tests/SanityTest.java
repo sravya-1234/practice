e10c1bfeb38af4e07fe70f9c93952152d3b49a47
unknown <sravya.wg164@gmail.com>
2018-10-14 13:55:05 -0700

    adding sanity test

---
+++ src/Tests/SanityTest.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SanityTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+				driver.get("http://www.seleniumhq.org");
+				
+				
+
+}
+}
---
+++ src/Tests/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author sravya
+ *
+ */
+package Tests;
